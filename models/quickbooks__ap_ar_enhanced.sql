677bbf7ea80596eb27b139529d6fc222a43ca3d6
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2021-04-13 10:32:43 -0500

    postgres changes

--- models/quickbooks__ap_ar_enhanced.sql
+++ models/quickbooks__ap_ar_enhanced.sql
@@ -20,10 +20,12 @@ departments as (
 ),
 {% endif %}
 
+{% if var('using_address', True) %}
 addresses as (
     select *
     from {{ref('stg_quickbooks__address')}}
 ),
+{% endif %}
 
 customers as (
     select *
@@ -41,16 +43,22 @@ final as (
         transaction_id,
         doc_number,
         cast(null as {{ dbt_utils.type_int() }}) as estimate_id, 
+
         {% if var('using_department', True) %}
         departments.fully_qualified_name as department_name,
         {% endif %}
+
         'vendor' as transaction_with,
         vendors.display_name as customer_vendor_name,
         vendors.balance as customer_vendor_balance,
+
+        {% if var('using_address', True) %}
         billing_address.city as customer_vendor_address_city,
         billing_address.country as customer_vendor_address_country,
-        vendors.web_url as customer_vendor_website,
         concat(billing_address.address_1, billing_address.address_2) as customer_vendor_address_line,
+        {% endif %}
+        
+        vendors.web_url as customer_vendor_website,
         cast(null as {{ dbt_utils.type_string() }}) as delivery_type,
         cast(null as {{ dbt_utils.type_string() }}) as estimate_status,
         total_amount,
@@ -78,9 +86,11 @@ final as (
     left join vendors
         on bill_join.vendor_id = vendors.vendor_id
     
+    {% if var('using_address', True) %}
     left join addresses as billing_address
         on vendors.billing_address_id = billing_address.address_id
-
+    {% endif %}
+    
     {% if var('using_invoice', True) %}
     union all
 
@@ -89,22 +99,28 @@ final as (
         invoice_join.transaction_id,
         doc_number,
         invoice_join.estimate_id,
+
         {% if var('using_department', True) %}
         departments.fully_qualified_name as department_name,
         {% endif %}
+
         'customer' as transaction_with,
         customers.fully_qualified_name as customer_vendor_name,
         customers.balance as customer_vendor_current_balance,
+
+        {% if var('using_address', True) %}
         billing_address.city as customer_vendor_address_city,
         billing_address.country as customer_vendor_address_country,
-        customers.website as customer_vendor_webiste,
         concat(billing_address.address_1, billing_address.address_2) as customer_vendor_address_line,
+        {% endif %}
+
+        customers.website as customer_vendor_webiste,
         invoice_join.delivery_type,
         invoice_join.estimate_status,
-        round(invoice_join.total_amount,2) as total_amount,
-        round(invoice_join.estimate_total_amount,2) as estimate_total_amount,
-        round(invoice_join.current_balance,2) as current_balance,
-        round(invoice_join.total_current_payment,2) as total_current_payment,
+        invoice_join.total_amount as total_amount,
+        invoice_join.estimate_total_amount as estimate_total_amount,
+        invoice_join.current_balance as current_balance,
+        invoice_join.total_current_payment as total_current_payment,
         invoice_join.due_date,
         case when invoice_join.current_balance != 0 and {{ dbt_utils.datediff("invoice_join.recent_payment_date", "invoice_join.due_date", 'day') }} < 0
             then true
@@ -123,8 +139,10 @@ final as (
         on invoice_join.department_id = departments.department_id
     {% endif %}
 
+    {% if var('using_address', True) %}
     left join addresses as billing_address
         on invoice_join.billing_address_id = billing_address.address_id
+    {% endif %}
 
     left join customers
         on invoice_join.customer_id = customers.customer_id
