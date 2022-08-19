fa9c85add6a90d68f3226e53c64828ded20d93af
Avinash Kunnath <avinash.kunnath@fivetran.com>
2022-08-19 11:08:11 -0700

    Add in Joe's review

--- models/quickbooks__general_ledger.sql
+++ models/quickbooks__general_ledger.sql
@@ -97,6 +97,7 @@ adjusted_gl as (
     select
         gl_union.transaction_id,
         index as transaction_index,
+        {{ dbt_utils.surrogate_key('gl_union.transaction_id', 'gl_union.index', 'accounts.name') }} as unique_id, 
         gl_union.transaction_date,
         gl_union.customer_id,
         gl_union.vendor_id,
