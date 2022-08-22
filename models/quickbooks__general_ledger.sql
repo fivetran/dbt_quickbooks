f7c18263b796d77220ce40bc3554cf9d1d2073f2
Avinash Kunnath <avinash.kunnath@fivetran.com>
2022-08-22 09:30:08 -0700

    move general ledger index to first field

--- models/quickbooks__general_ledger.sql
+++ models/quickbooks__general_ledger.sql
@@ -95,8 +95,8 @@ accounts as (
 
 adjusted_gl as (
     select
-        {{ dbt_utils.surrogate_key('gl_union.transaction_id', 'gl_union.index', 'accounts.name', ' gl_union.transaction_type') }} as unique_id,
         gl_union.index as transaction_index,
+        {{ dbt_utils.surrogate_key('gl_union.transaction_id', 'gl_union.index', 'accounts.name', ' gl_union.transaction_type') }} as unique_id,
         gl_union.transaction_date,
         gl_union.customer_id,
         gl_union.vendor_id,
