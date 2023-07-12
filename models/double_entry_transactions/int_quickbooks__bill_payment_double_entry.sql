1bc60cd02185df48b2ec8930382cb58dc24761db
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-07-11 21:20:33 -0500

    Update int_quickbooks__bill_payment_double_entry.sql

--- models/double_entry_transactions/int_quickbooks__bill_payment_double_entry.sql
+++ models/double_entry_transactions/int_quickbooks__bill_payment_double_entry.sql
@@ -40,7 +40,7 @@ bill_payment_join as (
         bill_payments.bill_payment_id as transaction_id,
         bill_payments.source_relation,
         row_number() over(partition by bill_payments.bill_payment_id, bill_payments.source_relation 
-            order by bill_payments.transaction_date) - 1 as index,
+            order by bill_payments.source_relation, bill_payments.transaction_date) - 1 as index,
         bill_payments.transaction_date,
         bill_payments.total_amount as amount,
         coalesce(bill_payments.credit_card_account_id,bill_payments.check_bank_account_id) as payment_account_id,
