3deb962e9a242c2db483861e1b0aaee6f1b7e61e
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-01-04 22:16:27 +0100

    bug/sales-item-account-id

--- models/double_entry_transactions/int_quickbooks__invoice_double_entry.sql
+++ models/double_entry_transactions/int_quickbooks__invoice_double_entry.sql
@@ -118,7 +118,7 @@ invoice_join as (
             when invoice_lines.discount_account_id is not null then 'DiscountLineDetail'
             when coalesce(invoice_lines.account_id, items.parent_income_account_id, items.income_account_id, invoice_lines.discount_account_id) is null then 'NoAccountMapping'
         end as invoice_line_transaction_type,
-        coalesce(invoice_lines.account_id, items.income_account_id, invoice_lines.discount_account_id) as account_id,
+        coalesce(invoice_lines.account_id, items.income_account_id, invoice_lines.discount_account_id, invoice_lines.sales_item_account_id) as account_id,
         {% endif %}
 
         coalesce(invoice_lines.sales_item_class_id, invoice_lines.discount_class_id, invoices.class_id) as class_id,
